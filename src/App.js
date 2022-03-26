e17688bb1372f7b66d80b025bb4529fe95f8c625
Mariam Bradai <mariambradai2@gmail.com>
2022-03-27 00:21:26 +0100

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Countries from "./pages/Countries";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Places from "./pages/Places";
+import PlacesDetails from "./pages/PlacesDetails";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 function App() {
   return (
-   <div>
-     <h1>Hi</h1>
-     <Button>OK</Button>
-   </div>
+  <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/login'component={Login} />
+      <Route path='/register' component={Register} />
+      <Route path='/profile' component={Profile} />
+      <Route exact path='/Countries' component={Countries}/>
+      <Route exact path='/Countries/Places' component={Places}/>
+      <Route path='/Countries/Places/:id' component={PlacesDetails} />
+
+    </Switch>
+  </Router>
   );
 }
 
---
+++ src/pages/PlacesDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlacesDetails() {
+  return (
+    <div>PlacesDetails</div>
+  )
+}
+
+export default PlacesDetails
